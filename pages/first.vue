b3fcc47456f25f5911b6c2ab2ff244f5f556b556
MasaKusuda <masa.kusuda.1999@gmail.com>
2020-06-12 14:58:39 +0900

    created first.vue

---
+++ pages/first.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <section class="hero is-primary is-large">
+      <div class="hero-body">
+        <div class="container has-text-centered ">
+          <p class="title is-size-1 has-text-weight-bold">
+            Grouping SNS
+          </p>
+          <p class="subtitle is-size-1">
+            welcome
+          </p>
+        </div>
+        <nav class="level">
+          <p class="level-item has-text-centered">
+            <a
+              href="./make_new_account"
+              class="link is-info has-text-black is-size-3"
+            >
+              Join the service
+            </a>
+          </p>
+          <p class="level-item has-text-centered">
+            <a href="./log_in" class="link is-info has-text-black is-size-3">
+              Login
+            </a>
+          </p>
+        </nav>
+      </div>
+    </section>
+  </div>
+</template>
