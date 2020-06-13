895167c6c595ec0f272ec03c901a41d79f72b47f
GON <hutabasuzukiryu@live.jp>
2020-06-14 00:21:00 +0900

    update: user.js

--- plugins/axios/modules/user.js
+++ plugins/axios/modules/user.js
@@ -4,6 +4,7 @@ import { axios } from '../index.js';
 export default {
   postUser(
     name,
+    bio,
     email,
     image,
     password,
@@ -11,14 +12,21 @@ export default {
     oauth_token_secret,
     tags
   ) {
-    return axios.$post(`users`, {
-      name,
-      email,
-      image,
-      password,
-      oauth_token,
-      oauth_token_secret,
-      tags,
+    const form = new FormData();
+    form.append('name', name);
+    form.append('bio', bio);
+    form.append('email', email);
+    if (image) form.append('image', image);
+    form.append('password', password);
+    form.append('oauth_token', oauth_token);
+    form.append('oauth_token_secret', oauth_token_secret);
+    tags.forEach((v, i) => {
+      form.append('tags[]', v);
+    });
+    return axios.$post(`users`, form, {
+      headers: {
+        'content-type': 'multipart/form-data',
+      },
     });
   },
 
@@ -37,15 +45,21 @@ export default {
     oauth_token_secret,
     tags
   ) {
-    return axios.$put(`users/${id}`, {
-      name,
-      bio,
-      email,
-      image,
-      password,
-      oauth_token,
-      oauth_token_secret,
-      tags,
+    const form = new FormData();
+    form.append('name', name);
+    form.append('bio', bio);
+    form.append('email', email);
+    if (image) form.append('image', image);
+    form.append('password', password);
+    form.append('oauth_token', oauth_token);
+    form.append('oauth_token_secret', oauth_token_secret);
+    tags.forEach((v, i) => {
+      form.append('tags[]', v);
+    });
+    return axios.$put(`users/${id}`, form, {
+      headers: {
+        'content-type': 'multipart/form-data',
+      },
     });
   },
 
