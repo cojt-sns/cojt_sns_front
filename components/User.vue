cdf9ce645ffeae71eba931987be6e53bcb959518
uutann <uutann9916@gmail.com>
2020-06-14 15:11:04 +0900

    userページデータ取得

--- components/User.vue
+++ components/User.vue
@@ -1,11 +1,41 @@
 <template>
   <div class="column is-fullheight section has-background-info">
-    User Content
+    <button class="button is-medium is-rounded">編集</button>
+    <div class="has-text-centered">{{ user.image }}</div>
+    <div class="title has-text-centered">{{ user.name }}</div>
+    <ul class="has-text-centered">
+      <li v-for="userTag in userTags" :key="userTag.id">#{{ userTag.name }}</li>
+    </ul>
+    <div class="has-text-centered">{{ user.bio }}</div>
+    <!-- userのtwitter情報 -->
   </div>
 </template>
 
 <script>
-export default {};
+import User from '@/plugins/axios/modules/user';
+export default {
+  data() {
+    return {
+      user: {},
+      userTags: {},
+      userTwitter: {},
+    };
+  },
+  created() {
+    User.getUser(this.$route.params.id).then((res) => {
+      this.user = res;
+    });
+    User.getUserTag(this.$route.params.id).then((res) => {
+      this.userTags = res;
+    });
+    // User.getUserTwitterProfile(this.$route.params.id).then((res)=>{
+    //   this.userTwitter=res;
+    // });
+  },
+  validate({ params }) {
+    // 数値でなければならない
+    return /^\d+$/.test(params.id);
+  },
+};
 </script>
-
 <style></style>
