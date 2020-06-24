3858f22c0c58899b6afbe4f1adc41f4880be8fc0
MasaKusuda <masa.kusuda.1999@gmail.com>
2020-06-24 14:06:54 +0900

    shaped User.vue

---
+++ components/TagTree.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="container">
+    <div></div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+<style scoped></style>
--- components/User.vue
+++ components/User.vue
@@ -1,20 +1,48 @@
 <template>
   <div class="column is-fullheight section has-background-info">
     <UserEditModal v-model="user_" :edit="edit" @close="closeModal()" />
-
     <!-- show profile -->
-    <button class="button is-medium is-rounded" @click="openModal()">
-      編集
-    </button>
-    <div class="image is-128x128 has-text-centered">
-      <img :src="user_.image" alt srcset />
+    <div class="section">
+      <div class="columns">
+        <div class="column">
+          <div class="box">
+            <div class="columns">
+              <div class="column is-2">
+                <div class="image is-128x128 has-text-centered">
+                  <img :src="user_.image" alt srcset />
+                </div>
+              </div>
+              <div class="column">
+                <div class="title has-text-centered username">
+                  {{ user_.name }}
+                </div>
+              </div>
+              <div class="column is-2 is-offser-10">
+                <button
+                  class="button is-medium is-rounded"
+                  @click="openModal()"
+                >
+                  編集
+                </button>
+              </div>
+            </div>
+            <div class="columns">
+              <div class="column">
+                <div class="has-text-centered is-size-4">
+                  Here comes user_.bio{{ user_.bio }}
+                </div>
+              </div>
+            </div>
+            <ul class="has-text-centered">
+              <li v-for="tag in user_.tags" :key="tag.id">
+                #{{ tag.fullname }}
+              </li>
+            </ul>
+            <!-- userのtwitter情報 -->
+          </div>
+        </div>
+      </div>
     </div>
-    <div class="title has-text-centered">{{ user_.name }}</div>
-    <ul class="has-text-centered">
-      <li v-for="tag in user_.tags" :key="tag.id">#{{ tag.fullname }}</li>
-    </ul>
-    <div class="has-text-centered">{{ user_.bio }}</div>
-    <!-- userのtwitter情報 -->
   </div>
 </template>
 
@@ -47,4 +75,15 @@ export default {
   },
 };
 </script>
-<style></style>
+
+<style lang="scss" scoped>
+.columns {
+  align-items: center;
+}
+.username {
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  -webkit-line-clamp: 1;
+  overflow: hidden;
+}
+</style>
