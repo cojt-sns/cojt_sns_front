8ed8d4ec675d6b42cc43f3eef2b93dc420d520f4
GON <hutabasuzukiryu@live.jp>
2020-07-15 15:24:33 +0900

    update: post

--- pages/groups/index.vue
+++ pages/groups/index.vue
@@ -7,7 +7,7 @@ import Main from '~/components/Main';
 import User from '@/plugins/axios/modules/user';
 import GroupUser from '@/plugins/axios/modules/groupUser';
 import Post from '@/plugins/axios/modules/post';
-// import Tag from '@/plugins/axios/modules/tag';
+import Group from '@/plugins/axios/modules/group';
 
 export default {
   components: {
@@ -15,11 +15,20 @@ export default {
   },
   async asyncData({ $auth }) {
     const groups = await User.getUserGroup($auth.user.id);
+    if (!groups.some((g) => g.id === 1)) groups.push(await Group.getGroup(1));
 
     const posts = [];
     const res = await Post.getGroupPost(1);
     for (const post of res) {
-      post.user = await GroupUser.getGroupUser(post.group_user_id);
+      if (post.group_user_id == null) {
+        post.user = {
+          name: '削除されたユーザー',
+          image: '/default.png',
+          user_id: null,
+        };
+      } else {
+        post.user = await GroupUser.getGroupUser(post.group_user_id);
+      }
       posts.push(post);
     }
 
