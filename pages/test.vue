561d9f1f03c413e96a63c03aaa813cf44677602b
uutann <uutann9916@gmail.com>
2020-07-15 14:35:45 +0900

    make logout modal testを見ると確認できます。 logoutごのリダイレクトはできてないです。

--- pages/test.vue
+++ pages/test.vue
@@ -1,62 +1,26 @@
 <template>
   <Main>
-    <div class="column is-fullheight">
-      <div v-for="(group, index) in groups" :key="group.id">
-        <a @click="modalStates.splice(index, 1, true)"
-          >#{{ group.tags.map((tag) => tag.fullname).join('#') }}</a
-        >
-        <GroupJoinModal
-          :group="group"
-          :active="modalStates[index]"
-          @close="modalStates.splice(index, 1, false)"
-        />
-      </div>
-      <div>
-        <button @click="show = true">AddTagModal</button>
-        <AddTagModal :tags="Tags[4]" :active="show" @close="show = false" />
-      </div>
-    </div>
+    <LogoutModal :open="logout" @close="switchLogoutModal" />
   </Main>
 </template>
 
 <script>
 import Main from '~/components/Main';
-import GroupJoinModal from '~/components/GroupJoinModal';
-import AddTagModal from '~/components/AddTagModal';
-import Group from '@/plugins/axios/modules/group';
-// import Tag from '@/plugins/axios/modules/tag';
-
+import LogoutModal from '~/components/LogoutModal';
 export default {
   components: {
     Main,
-    GroupJoinModal,
-    AddTagModal,
-  },
-  async asyncData() {
-    const groups = [];
-    const modalStates = [];
-    const search = await Group.searchGroup();
-    const Tags = [];
-    for (const group of search) {
-      const tags = [];
-      // for (const id of group.tags) {
-      //   tags.push(await Tag.getTag(id));
-      //   Tags.push(await Tag.getTag(id));
-      // }
-      group.tags = tags;
-      groups.push(group);
-      modalStates.push(false);
-    }
-    return {
-      groups,
-      modalStates,
-      Tags,
-    };
+    LogoutModal,
   },
   data() {
     return {
-      show: false,
+      logout: true,
     };
+  },
+  methods: {
+    switchLogoutModal() {
+      this.logout = !this.logout;
+    },
   },
 };
 </script>
