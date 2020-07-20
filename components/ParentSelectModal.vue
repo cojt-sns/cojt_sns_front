d44ec4018dfaacce1bfc701d746afe9afbd14dee
k-usuha <kouta528bado@yahoo.co.jp>
2020-07-20 11:50:58 +0900

    add: ParentSelectModal

---
+++ components/ParentSelectModal.vue
@@ -0,0 +1,131 @@
+<template>
+  <Modal
+    :class="{ 'is-active': open }"
+    :title="'親グループを選択'"
+    @close="$emit('close')"
+  >
+    <div class="wr">
+      <div class="field has-addons">
+        <div class="control is-expanded">
+          <input
+            v-model="query"
+            type="text"
+            class="input"
+            @keydown.enter="searchGroups()"
+          />
+        </div>
+        <div class="control">
+          <!-- eslint-disable no-irregular-whitespace -->
+          <button class="button" @click="searchGroups()">
+            Search
+          </button>
+        </div>
+      </div>
+      <aside class="menu">
+        <ParentSelectBar
+          v-for="group in groups"
+          :key="group.id"
+          :group="group"
+          :target_group_id="group_id"
+          :available="true"
+        />
+      </aside>
+    </div>
+  </Modal>
+</template>
+
+<script>
+import Group from '@/plugins/axios/modules/group';
+import Modal from '@/components/Modal';
+import ParentSelectBar from '@/components/ParentSelectBar';
+
+export default {
+  components: {
+    Modal,
+    ParentSelectBar,
+  },
+  props: {
+    open: {
+      type: Boolean,
+      required: true,
+    },
+    group_id: {
+      type: Number,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      name: '',
+      error: '',
+      groups: [],
+      query: '',
+    };
+  },
+  mounted() {
+    // this.getGroups();
+  },
+  watch: {
+    open(newValue) {
+      if (newValue && this.groups.length <= 0) {
+        this.getGroups();
+      }
+    },
+  },
+  methods: {
+    async searchGroups() {
+      this.groups = [];
+      this.groups = await Group.searchGroup(this.query, null, 1);
+    },
+
+    UpdateParent() {
+      Group.putGroup(this.query);
+    },
+
+    async getGroups() {
+      try {
+        this.groups = await Group.searchGroup(null, null, -1);
+      } catch (error) {
+        this.error = error.data.message.name[0];
+      }
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.modal-card {
+  overflow: visible;
+  .modal-card-body {
+    overflow: visible;
+  }
+}
+.wr {
+  max-height: 60vh;
+  overflow: scroll;
+  .menu-list {
+    .level {
+      border: 1px solid #dddddd;
+      padding: 5px 10px;
+      margin: 0;
+
+      &:nth-child(even) {
+        border-top: none;
+        border-bottom: none;
+      }
+
+      .level-left {
+        visibility: hidden;
+      }
+
+      &:hover {
+        background-color: #eee;
+      }
+
+      &:hover > .level-left {
+        visibility: visible;
+      }
+    }
+  }
+}
+</style>
--- components/Posts.vue
+++ components/Posts.vue
@@ -2,7 +2,6 @@
   <div class="column is-fullheight">
     <GroupEditModal
       v-model="group_"
-      :whichmodal="WhichModal"
       :open="edit"
       @close="SwitchGroupEditModal"
     />
@@ -10,6 +9,11 @@
       :group="group_"
       :open="exit"
       @close="SwitchGroupExitModal"
+    />
+    <ParentSelectModal
+      :group_id="group_.id"
+      :open="parentSelect"
+      @close="SwitchParentSelectModal"
     />
     <div class="header has-background-primary">
       <nav class="level">
@@ -34,6 +38,9 @@
             <div class="dropdown-content">
               <a href="#" class="dropdown-item" @click="SwitchGroupEditModal"
                 >編集</a
+              >
+              <a href="#" class="dropdown-item" @click="SwitchParentSelectModal"
+                >親グループを選択</a
               >
               <a class="dropdown-item">他の人を招待する</a>
               <a href="#" class="dropdown-item">Twitterで共有</a>
@@ -117,6 +124,7 @@
 import PostComponent from '~/components/Post';
 import GroupExitModal from '~/components/GroupExitModal';
 import GroupEditModal from '~/components/GroupEditModal';
+import ParentSelectModal from '~/components/ParentSelectModal';
 import Post from '@/plugins/axios/modules/post';
 import GroupUser from '@/plugins/axios/modules/groupUser';
 export default {
@@ -124,6 +132,7 @@ export default {
     Post: PostComponent,
     GroupExitModal,
     GroupEditModal,
+    ParentSelectModal,
   },
   props: {
     posts: {
@@ -151,6 +160,7 @@ export default {
       ),
       row: 1,
       exit: false,
+      parentSelect: false,
     };
   },
   mounted() {
@@ -194,6 +204,9 @@ export default {
     },
     SwitchGroupExitModal() {
       this.exit = !this.exit;
+    },
+    SwitchParentSelectModal() {
+      this.parentSelect = !this.parentSelect;
     },
     adjustHeight() {
       const textarea = this.$refs?.adjustTextarea;
