c1a22b004063828e16d665a43c1717140dfd7089
GON <hutabasuzukiryu@live.jp>
2020-07-30 08:06:34 +0900

    update: error

--- layouts/error.vue
+++ layouts/error.vue
@@ -2,13 +2,19 @@
   <main class="container">
     <section class="section has-text-centered">
       <h1 class="title">{{ error.statusCode }}</h1>
-      <p class="subtitle has-text-grey m-t-30">
+      <p v-if="error.statusCode === 404" class="subtitle has-text-grey m-t-30">
         指定されたページが見つかりませんでした。<br />
         既に削除されたか、URLが間違っている可能性があります。
       </p>
+      <p v-else class="subtitle has-text-grey m-t-30">
+        {{ error.data }}
+      </p>
       <div class="columns has-text-centered">
         <div class="column">
-          <nuxt-link to="/groups" class="button is-primary">
+          <nuxt-link
+            :to="error.statusCode === 404 ? '/groups' : '/'"
+            class="button is-primary"
+          >
             ホームに戻る
           </nuxt-link>
         </div>
